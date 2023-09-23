202a0922da6b7907971443142eee82b251298832
manoj patil <manojpatil9147@gmail.com>
2023-09-23 19:19:38 +0530

    connected chatbot_model

--- playground/views.py
+++ playground/views.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse
 from django.core.files import File
 from .logic import botResponse 
 from .forms import PostBot
+from chatbot_model import model1
 
 # Create your views here.  request handler
 def index(request):
     a=""
     if request.method=="POST":
-        a=request.POST.get("entryField")
+        val=request.POST.get("entryField")
+        a=model1.getAnswer(val)
     return render(request,"index.html",{"formValue":a})
 
 def login(request):
