2365c1640bc38b5acc0a7e0caa594ce03c69652e
PranavKadagadakai <pkadagadakai@gmail.com>
2024-01-19 20:24:45 +0530

    Some changes

--- playground/views.py
+++ playground/views.py
@@ -1,26 +1,26 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse 
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.core.files import File
-from .logic import botResponse 
+from .logic import botResponse
 from .forms import PostBot
 from chatbot_model import model1
 
+
 # Create your views here.  request handler
 def index(request):
-    data="Bot:  Hey!! I am your chatbot. Ask me anything about mining laws!!😊\n"
-    if request.method=="POST":
-        question=request.POST.get("entryField")
-        data=request.POST.get("data")
-        ans=model1.getAnswer(question)
-        data += "\n" + "User: " + question + "\n" + "Bot:  " + ans+"\n"
+    data = "Bot:  Hey!! I am your Tele-Med KIOSK. Tell me your symptoms and I will do the diagnosis!!\n"
+    if request.method == "POST":
+        question = request.POST.get("entryField")
+        data = request.POST.get("data")
+        ans = model1.getAnswer(question)
+        data += "\n" + "User: " + question + "\n" + "Bot:  " + ans + "\n"
         print("data:", data)
-    return render(request,"index.html",{"formValue":data})
+    return render(request, "index.html", {"formValue": data})
+
 
 def login(request):
-    return render(request,"login.html")
+    return render(request, "login.html")
+
 
 def demo(request):
-    return render(request,"demo.html")
-
-
-    
+    return render(request, "demo.html")
