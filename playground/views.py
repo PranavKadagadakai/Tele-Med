ee171e5e0492af1065a4c1cb8b3e6d23789e7469
PranavKadagadakai <pkadagadakai@gmail.com>
2024-01-22 20:01:30 +0530

    Model name and module

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.core.files import File
 from .logic import botResponse
 from .forms import PostBot
-from chatbot_model import model1
+from TeleMed_model import model1
 
 
 # Create your views here.  request handler
