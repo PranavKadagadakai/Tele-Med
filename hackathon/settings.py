8f1515f4e0b01797557da0f4049d0ef9155834dd
PranavKadagadakai <pkadagadakai@gmail.com>
2024-01-28 17:13:45 +0530

    Some more changes

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -124,3 +124,8 @@ MEDIA_ROOT=BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Added manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
